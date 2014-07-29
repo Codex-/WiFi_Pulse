298f16601d4d3c5334d8f0afd321ac4bf8383111
Codex- <codex.nz@gmail.com>
2014-07-29 21:34:37 +1200

    Tidy up
    Cleaned up syntax a little

--- WiFi_Pulse.sh
+++ WiFi_Pulse.sh
@@ -1,44 +1,38 @@
 #!/bin/bash
-
-  #WiFi Pulse
-
-  #Will check active status of defined wlan and reconnect if link is down
-
-  #  Install to /usr/local/bin/WiFi_Pulse
-
+  # WiFi Pulse
+  # Will check active status of defined wlan and reconnect if link is down
+  #  Install to /usr/local/bin/WiFi_Pulse.sh
   #  CHMOD 0755
-
   #  Add to cronjob
 
-
-  #Set wlan to check
+  # Set wlan to check
 wlan='wlan0'
 
 
-  #Start script
+  # Start script
 echo "$(tput setaf 6)Beginning WiFi Pulse task $(tput sgr 0)"
 echo "Interface selected: $(tput setaf 5)$wlan $(tput sgr 0)"
 echo
 
 
-  #Lock task
+  # Lock task
 echo "Locking task to prevent duplicate execution..."
 
 (
-  # Wait for lock on /var/lock/.wifi_pulse.lock (fd 200) for 10 seconds
+  # Wait for flock on task (fd 200) for 10 seconds
   flock -x -w 10 200 ||
 if [ "$?" != "0"]; then
   echo "$(tput setaf 1)$(tput setab 7)ERROR$(tput sgr 0)$(tput setaf 1) Unable to lock. $(tput sgr 0)";
 else
   exit 1;
 fi;
-  #backward lockdir compatibility
+  # Backward lockdir compatibility
 echo $$>>/var/lock/.wifi_pulse.lock
   
 echo "$(tput setaf 2)Lock successful! $(tput sgr 0)"
 echo
 
-  #Start WiFi check	
+  # Start WiFi check	
 echo "Performing connectivity check for $(tput setaf 5)$wlan$(tput sgr 0)..."
 if ifconfig $wlan | grep -q "inet addr:" ; then
   echo "$(tput setaf 5)$wlan$(tput sgr 0): $(tput setaf 2)Active$(tput sgr 0). Network online."
